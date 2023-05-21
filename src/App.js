df42079ec9e42d7d75cf91fcc20194d67c5072ef
tregubchenko <tregubchenko2@yandex.ru>
2023-05-21 13:14:09 +0300

    Header init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navbar from "./component/Navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h2>Test hello!</h2>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+      <React.Fragment>
+          <Navbar/>
+      </React.Fragment>
+  )
 }
 
-export default App;
+export default App
---
+++ src/component/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navbar = () => {
+    return(
+        <header>
+            <div className="logo">
+                Logo
+            </div>
+            <h3>Logo</h3>
+            <h3>User</h3>
+        </header>
+    )
+}
+
+export default Navbar
